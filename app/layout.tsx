2811b5a143b34597ac1d7216089c9d2dafb3fe59
Shreyas M S <shreyasmadappady20@gmail.com>
2024-09-21 20:16:37 +0530

    c

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import NextTopLoader from "nextjs-toploader";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -36,8 +34,6 @@ export default function RootLayout({
         <NextTopLoader showSpinner={false} color="#0050f0" crawlSpeed={50} />
         <Navbar />
         {children}
-        <Analytics />
-        <SpeedInsights />
         <Footer />
       </body>
     </html>
