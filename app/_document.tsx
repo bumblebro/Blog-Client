ec56880cefc9a676520bf8aa104f74bc778c3abd
Shreyas M S <shreyasmadappady20@gmail.com>
2024-09-27 00:18:59 +0530

    c

---
+++ app/_document.tsx
@@ -0,0 +1,11 @@
+<script
+  data-partytown-config
+  dangerouslySetInnerHTML={{
+    __html: `
+          partytown = {
+            lib: "/_next/static/~partytown/",
+            forward: ["gtag"]           
+          };
+        `,
+  }}
+/>;
