5bae920afa99a978676d0af550e1c6279dbac310
Shreyas M S <shreyasmadappady20@gmail.com>
2025-02-18 11:03:44 +0530

    c

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -132,7 +132,7 @@ export async function GET(req: NextRequest) {
         style={{
           flexGrow: 1, // Ensures the image takes available space
           width: "100%",
-          objectFit: "contain",
+          objectFit: "cover",
           objectPosition: "center",
         }}
       />
