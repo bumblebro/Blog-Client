ffd53b31ed6d37f633cb41ce39a1c051cd78ff92
Shreyas M S <shreyasmadappady20@gmail.com>
2025-02-24 23:49:17 +0530

    c

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -60,6 +60,83 @@ export async function GET(req: NextRequest) {
   const num = searchParams.get("num") || "3";
 
   const templates = [
+    // <div
+    //   key={"1"}
+    //   style={{
+    //     height: "100vh", // Fixed height for the container
+    //     width: "100%",
+    //     display: "flex",
+    //     flexDirection: "column",
+    //     alignItems: "center",
+    //     justifyContent: "flex-start",
+    //     backgroundColor: "black",
+    //     // backgroundImage: `url(${cover})`,
+    //   }}
+    // >
+    //   {/* <div
+    //     style={{
+    //       // height: "200px", // Fixed height for title
+    //       // paddingTop: "5px",
+    //       // paddingBottom: "25px",
+    //       color: "white",
+    //       textAlign: "center",
+    //       paddingRight: "10px",
+    //       paddingLeft: "10px",
+    //       fontSize: "120px",
+    //       fontWeight: "900",
+    //       fontFamily: "source-sans-pro.black",
+    //       textTransform: "uppercase",
+    //     }}
+    //   >
+    //     {phrases[Math.floor(Math.random() * phrases.length)]}
+    //   </div>{" "} */}
+    //   <div
+    //     style={{
+    //       // height: "200px", // Fixed height for title
+    //       paddingTop: "25px",
+    //       paddingBottom: "25px",
+    //       color: "white",
+    //       textAlign: "center",
+    //       paddingRight: "10px",
+    //       paddingLeft: "10px",
+    //       fontSize: "90px",
+    //       // fontSize: "120px",
+    //       // fontFamily: "SoinSansPro-Bold",
+    //       fontFamily: "source-sans-pro.black",
+    //       textTransform: "capitalize",
+    //     }}
+    //   >
+    //     {DeSlugify(title)}
+    //   </div>{" "}
+    //   <div
+    //     style={{
+    //       // height: "60px", // Fixed height for footer text
+    //       textAlign: "center",
+    //       fontSize: "50px",
+    //       color: "black",
+    //       paddingRight: "30px",
+    //       paddingLeft: "30px",
+    //       fontStyle: "normal",
+    //       backgroundColor: "#FFFFF7",
+    //       fontWeight: 100,
+    //       fontFamily: "source-sans-pro.extralight",
+    //     }}
+    //   >
+    //     wordofmany.com
+    //   </div>
+    //   <img
+    //     src={cover}
+    //     alt="test"
+    //     height={900}
+    //     width={1000}
+    //     style={{
+    //       flexGrow: 1, // Ensures the image takes available space
+    //       width: "100%",
+    //       objectFit: "cover",
+    //       objectPosition: "center",
+    //     }}
+    //   />
+    // </div>,
     <div
       key={"1"}
       style={{
@@ -73,7 +150,7 @@ export async function GET(req: NextRequest) {
         // backgroundImage: `url(${cover})`,
       }}
     >
-      {/* <div
+      <div
         style={{
           // height: "200px", // Fixed height for title
           // paddingTop: "5px",
@@ -82,27 +159,25 @@ export async function GET(req: NextRequest) {
           textAlign: "center",
           paddingRight: "10px",
           paddingLeft: "10px",
-          fontSize: "120px",
+          fontSize: "80px",
           fontWeight: "900",
           fontFamily: "source-sans-pro.black",
           textTransform: "uppercase",
         }}
       >
         {phrases[Math.floor(Math.random() * phrases.length)]}
-      </div>{" "} */}
+      </div>{" "}
       <div
         style={{
           // height: "200px", // Fixed height for title
-          paddingTop: "25px",
+          // paddingTop: "25px",
           paddingBottom: "25px",
           color: "white",
           textAlign: "center",
           paddingRight: "10px",
           paddingLeft: "10px",
-          fontSize: "90px",
-          // fontSize: "120px",
-          // fontFamily: "SoinSansPro-Bold",
-          fontFamily: "source-sans-pro.black",
+          fontSize: "50px",
+          fontFamily: "SoinSansPro-Bold",
           textTransform: "capitalize",
         }}
       >
@@ -112,7 +187,7 @@ export async function GET(req: NextRequest) {
         style={{
           // height: "60px", // Fixed height for footer text
           textAlign: "center",
-          fontSize: "50px",
+          fontSize: "30px",
           color: "black",
           paddingRight: "30px",
           paddingLeft: "30px",
@@ -144,8 +219,10 @@ export async function GET(req: NextRequest) {
   // const randomTemplate = templates[parseInt(0)];
 
   return new ImageResponse(randomTemplate, {
+    // width: 1000,
+    // height: 2000,
     width: 1000,
-    height: 2000,
+    height: 1500,
 
     fonts: [
       {
